71da891c007fb85333ddfd17c336328ff3ed8037
unknown <raimundo.j.ferreira0@gmail.com>
2022-05-05 22:19:15 -0300

    correção

--- Codes/Colisão 2D.py
+++ Codes/Colisão 2D.py
@@ -1,5 +1,5 @@
 def colisão (x1,y1,x2,y2,xa,ya,xb,yb):
     """Detecta a colisão entre dois retângulos"""
-    if((x2<xa) and (y2<ya)):
-        return "Não houve colisão "
-    return " Houve colisão "
+    if((x2<xa) or (y2<ya) or (x1>xb) or (y1>yb)):
+        return "Não houve colisão"
+    return " Houve colisão"
