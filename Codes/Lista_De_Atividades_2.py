c3a4aa60b45f293b5bb26665dc1b02ce55ed9f45
unknown <raimundo.j.ferreira0@gmail.com>
2022-05-21 15:13:43 -0300

    exercicio 5

--- Codes/Lista_De_Atividades_2.py
+++ Codes/Lista_De_Atividades_2.py
@@ -42,3 +42,39 @@ float, float, float -> float"""
         # Segundo a matemática para delta > 0 existe 2 raizes reais.
         return ((-b)+ math.sqrt(delta))/2*a , ((-b)- math.sqrt(delta))/2*a
 
+#Exercício 3 - Lab3_IDLE_v2.pdf
+
+def Repeticao_de_textos(texto, n):
+    """Retorna n repetições do texto de entrada.
+string, int -> string"""
+    return (texto + " ") * n
+
+#Exercício 4 - Lab3_IDLE_v2.pdf
+
+def notacao_data (dia, mes, ano):
+    """Com base no dia, mes e ano retorna a forma de notação padrão para data, dado por:
+dia/mês/ano.
+int, int, int -> string"""
+    notacao = str(dia) + "/" + str(mes) + "/" + str(ano)
+    return notacao
+
+#Exercício 5 - Lab3_IDLE_v2.pdf
+
+def funcao_exercicio (x):
+    """retorna o valor de f(x) = y, para a função apresentada no exercício, que possui
+diferentes formas para diferentes valores de x.
+float -> float"""
+    if(x < 0):
+        return 0
+    elif(0 <= x <=2):
+        return x
+    elif(2 < x <= 3.5):
+        return 2
+    elif(3.5 < x <= 5):
+        return 3
+    elif(x > 5):
+        return (x**2) - (10*x) + 28
+#Como temos uma função com 5 intervalos, e para cada intervalo um condicional diferente
+#é necessario então no mínimo 5 testes, um no intervalo de cada condicional, para saber
+#se a função funciona corretamente.
+
