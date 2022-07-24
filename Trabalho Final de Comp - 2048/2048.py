45f8e4ec857dcf8bc876656665fe4f0c837a2ddd
unknown <raimundo.j.ferreira0@gmail.com>
2022-07-24 12:12:27 -0300

    atualização

--- Trabalho Final de Comp - 2048/2048.py
+++ Trabalho Final de Comp - 2048/2048.py
@@ -33,6 +33,65 @@ matriz -> none"""
         tab[linha][casa] = random.choice([2,2,4,2,2])
     return tab
 
+def mostra_tabuleiro(tab):
+    """Uma função que recebe uma matriz (que representa o tabuleiro) e realiza uma "impressão"
+do mesmo na tela em formato de tabuleiro"""
+    tabuleiro = []
+    for linha in tab:
+        for elemento in linha:
+            tabuleiro = tabuleiro + [elemento]
+    impressao = str(tabuleiro[:4]) + "\n" + str(tabuleiro[4:8]) + "\n" + str(tabuleiro [8:12]) + "\n" + str(tabuleiro[12:])
+    print(impressao)
+
+def direita(tab):
+    """ Recebendo um tabuleiro realiza a movimentação do mesmo para a direita"""
+    pode_ir = False
+    somar = False
+    rodar = 0
+    repetir = 2
+    
+    for linha in tab:
+        while(rodar < repetir):
+            for a in range(0,4):
+                #Etapas de verificação
+                if(a != 3):
+                    if(linha[a+1] == 0):
+                        pode_ir = True
+                    else:
+                        pode_ir = False
+                    if(linha[a+1] == linha[a]):
+                        somar = True
+                    else:
+                        somar = False
+                else:
+                    pode_ir = False
+                    somar = False
+
+                #Realiza o movimento
+                if(pode_ir == True):
+                    linha[a+1] = linha[a]
+
+                #Realiza a soma dos números caso eles sejam iguais
+                if(somar == True):
+                    linha[a+1] = linha[a] + linha[a+1]
+
+                #Remove os 0
+                if(pode_ir == True) or (somar == True):
+                    linha[a] = 0
+
+                #reinicia o for
+                if(a == 3):
+                    rodar = rodar + 1
+        if(rodar == repetir):
+            rodar = 0
+    mostra_tabuleiro(tab)
+            
+
+
+    
+
+    
+
 
 
 
